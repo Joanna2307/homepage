9d9ce7e174eae351109ecfbc4421de2cffb08723
Joanna Gorniak <joanna.gorniak.00@gmail.com>
2024-11-05 13:16:34 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("Siemka, co tam u was słychać?");
+
+let przycisk = document.querySelector(".przycisk");
+let zdj = document.querySelector(".zdj")
+przycisk.addEventListener ("click", ()=>{
+
+zdj.remove();
+});
+console.log=(przycisk);
